0379457b2b5941103ad0dfb57176a2c52b701de4
BBSimsek1 <b.berkinsimsek@hotmail.com>
2026-09-02 14:15:30 +0300

    Move loading and validation helpers to app loader

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from data_loader import load_purchase_requests
-from validator import validate_purchase_requests
 from risk_analyzer import add_risk_status_to_all, calculate_risk_summary, calculate_department_summary, calculate_amount_summary_by_field
 from data_writer import save_report_data
 from reporter import show_purchase_report
 from logger import write_log
-from config_loader import load_config
-from config_validator import validate_config
 from datetime import datetime
 from report_builder import build_report_data
+from app_loader import (
+    load_and_validate_config,
+    load_and_validate_purchase_requests
+)
 from constants import (
     INPUT_FILE_PATH,
     OUTPUT_FILE_PATH,
@@ -87,56 +87,6 @@ def generate_purchase_risk_report(purchase_requests, very_risky_limit, risky_lim
     )
 
     save_report_data(OUTPUT_FILE_PATH, report_data)
-
-
-def load_and_validate_config():
-    config = load_config(CONFIG_FILE_PATH)
-
-    if len(config) == 0:
-        print("Rapor oluşturulamadı: config dosyası okunamadı.")
-        write_log(LOG_FILE_PATH, LOG_LEVEL_ERROR, "Rapor oluşturulamadı: config dosyası okunamadı.")
-        return None
-
-    is_config_valid, config_errors = validate_config(config)
-
-    if not is_config_valid:
-        print("Config doğrulama hataları:")
-        print("--------------------------")
-
-        for error in config_errors:
-            print(error)
-            write_log(LOG_FILE_PATH, LOG_LEVEL_ERROR, error)
-
-        print("Rapor oluşturulamadı: config dosyası hatalı.")
-        write_log(LOG_FILE_PATH, LOG_LEVEL_ERROR, "Rapor oluşturulmadı: config dosyası hatalı.")
-        return None
-
-    return config
-    
-
-def load_and_validate_purchase_requests():
-    purchase_requests = load_purchase_requests(INPUT_FILE_PATH)
-
-    if len(purchase_requests) == 0:
-        print("Rapor oluşturulamadı: satın alma talebi bulunamadı.")
-        write_log(LOG_FILE_PATH, LOG_LEVEL_WARNING, "Rapor oluşturulamadı: satın alma talebi bulunamadı.")
-        return None
-
-    is_valid, validation_errors = validate_purchase_requests(purchase_requests)
-
-    if not is_valid:
-        print("Veri doğrulama hataları:")
-        print("------------------------")
-
-        for error in validation_errors:
-            print(error)
-            write_log(LOG_FILE_PATH, LOG_LEVEL_ERROR, error)
-
-        print("Rapor oluşturulamadı: veri formatı eksik veya hatalı.")
-        write_log(LOG_FILE_PATH, LOG_LEVEL_ERROR, "Rapor oluşturulamadı: veri formatı eksik veya hatalı.")
-        return None
-
-    return purchase_requests
 
 
 def main():

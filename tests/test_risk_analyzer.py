14780fc386a62ad9af51f292341b123dda6ad3c0
BBSimsek1 <b.berkinsimsek@hotmail.com>
2026-09-05 14:40:44 +0300

    Add risk summary test

--- tests/test_risk_analyzer.py
+++ tests/test_risk_analyzer.py
@@ -1,10 +1,17 @@
-from risk_analyzer import add_risk_status
+from risk_analyzer import (
+    add_risk_status,
+    calculate_risk_summary
+)
 from constants import (
     FIELD_AMOUNT,
     FIELD_RISK_STATUS,
     RISK_STATUS_NORMAL,
     RISK_STATUS_RISKY,
     RISK_STATUS_VERY_RISKY,
+    SUMMARY_TOTAL_COUNT,
+    SUMMARY_NORMAL_COUNT,
+    SUMMARY_RISKY_COUNT,
+    SUMMARY_VERY_RISKY_COUNT
 )
 
 
@@ -48,3 +55,24 @@ def test_very_risky_request():
     )
 
     assert updated_request[FIELD_RISK_STATUS] == RISK_STATUS_VERY_RISKY
+
+
+def test_calculate_risk_summary():
+    updated_requests = [
+        {
+            FIELD_RISK_STATUS: RISK_STATUS_NORMAL
+        },
+        {
+            FIELD_RISK_STATUS: RISK_STATUS_RISKY
+        },
+        {
+            FIELD_RISK_STATUS: RISK_STATUS_VERY_RISKY
+        },
+    ]
+
+    summary = calculate_risk_summary(updated_requests)
+
+    assert summary[SUMMARY_TOTAL_COUNT] == 3
+    assert summary[SUMMARY_NORMAL_COUNT] == 1
+    assert summary[SUMMARY_RISKY_COUNT] == 1
+    assert summary[SUMMARY_VERY_RISKY_COUNT] == 1
